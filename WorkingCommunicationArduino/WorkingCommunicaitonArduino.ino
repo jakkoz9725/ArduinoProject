56612815c25be72ddc18a96a675ed303a088ab85
Jakub Kozłowski <jakkoz9725@gmail.com>
2018-11-27 21:58:02 +0100

    Firebase added

--- WorkingCommunicationArduino/WorkingCommunicaitonArduino.ino
+++ WorkingCommunicationArduino/WorkingCommunicaitonArduino.ino
@@ -1,4 +1,14 @@
 #include <SoftwareSerial.h>
+#include <MFRC522.h>
+#include <Wire.h>
+#include <SPI.h>
+
+const byte Jakub[] = {0xd3, 0xc8, 0x12, 0x1c};
+const byte Agnieszka[] = {0x69, 0x2A, 0xD6, 0x73};
+
+MFRC522 rfid(10, 9);
+MFRC522::MIFARE_Key key;
+
 int rpm;
 SoftwareSerial DataSerial(2,3); //Rx,Tx
 void setup() {
@@ -7,35 +17,57 @@ void setup() {
  pinMode(6,INPUT); // INPUT DALEKI
  pinMode(5,OUTPUT); // OUTPUT DALEKI
 
+SPI.begin();
+rfid.PCD_Init();
 DataSerial.begin(9600);
 Serial.begin(9600);
 }
 
 void loop(){
+if (rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial())
+{
+if (rfid.uid.uidByte[0] == Jakub[0] &&
+rfid.uid.uidByte[1] == Jakub[1] &&
+rfid.uid.uidByte[2] == Jakub[2] &&
+rfid.uid.uidByte[3] == Jakub[3])
+ {
+Dobra(3);
+ }else if (rfid.uid.uidByte[0] == Agnieszka[0] &&
+rfid.uid.uidByte[1] == Agnieszka[1] &&
+rfid.uid.uidByte[2] == Agnieszka[2] &&
+rfid.uid.uidByte[3] == Agnieszka[3])
+  {
+Dobra(4);
+rfid.PICC_HaltA();
+rfid.PCD_StopCrypto1();
+  }
+}
+
   if(digitalRead(6) == HIGH){
     digitalWrite(5,HIGH);
       while(digitalRead(6) == HIGH){
-        delay(50);
         char value[2];
         DataSerial.readBytes(value,2);
         rpm = atoi(value);
         if(rpm == 2){
           digitalWrite(8,HIGH);
           digitalWrite(5,LOW);
-          delay(1000);
+          delay(2000);
           digitalWrite(8,LOW);
         }
       }
   }
-  if(digitalRead(7) == HIGH){
-    digitalWrite(5,HIGH);
-    digitalWrite(8,HIGH); //dioda
-    while(digitalRead(6) == HIGH){
-      DataSerial.println(3);
-      delay(50);
-    }
-  }else{
     digitalWrite(5,LOW);
     digitalWrite(8,LOW);
-  }
 }
+void Dobra(int whosThere){
+    digitalWrite(5,HIGH);
+    digitalWrite(8,HIGH); //dioda
+    delay(2000);
+    Serial.println("test");
+    while(digitalRead(6) == HIGH){
+      DataSerial.println(whosThere);
+      delay(50);
+    }
+  }
+
--- WorkingCommunicationWIFI/WorkingCommunicationWIFI.ino
+++ WorkingCommunicationWIFI/WorkingCommunicationWIFI.ino
@@ -1,27 +1,56 @@
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <FirebaseArduino.h>
+#include <time.h>
+
+
+// Set these to run example.
+#define FIREBASE_HOST "xxx"
+#define FIREBASE_AUTH "xxx"
+#define WIFI_SSID "xxx"
+#define WIFI_PASSWORD "xxx"
+
+int redlight;
 int rpm;
 SoftwareSerial DataSerial(D2,D3); //Rx,Tx
 void setup() {
- pinMode(D8,INPUT); // PRZYCISK
  pinMode(D7,OUTPUT); // DIODA
  pinMode(D6,OUTPUT); //OUTPUT DALEKI
  pinMode(D5,INPUT); // INPUT DALEKI
 DataSerial.begin(9600);
 Serial.begin(9600);
+WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+  }
+  Serial.println("Wifi connected sucessful");
+  configTime(3600, 0, "pool.ntp.org", "time.nist.gov");
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+  Firebase.set("Zamek",0);
+  Firebase.set("WhosThere","Unknown");
+  Firebase.pushString("Historia","prepare");
 }
 
 void loop() {
-  if(digitalRead(D8) == HIGH){
+  if(Firebase.getInt("Zamek") == 1){
+    while(Firebase.getInt("Zamek") == 1){
     digitalWrite(D6,HIGH);
     digitalWrite(D7,HIGH); //dioda
     while(digitalRead(D5) == HIGH){
-      delay(50);
       DataSerial.println(2);
+      Firebase.setInt("Zamek",0);
+      redlight = 0;
+      Firebase.setInt("Zamek",0);
     }
-  }else{
-    digitalWrite(D6,LOW);
-    digitalWrite(D7,LOW);
   }
+  time_t now = time(nullptr);
+  String nazwa_otwierajacego = Firebase.getString("WhosThere");
+  String data_otwierajacy = ctime(&now) + nazwa_otwierajacego;
+  Firebase.pushString("Historia",data_otwierajacy);
+  digitalWrite(D6,LOW);
+  digitalWrite(D7,LOW);
+  }
+  
 if(digitalRead(D5) == HIGH){
   digitalWrite(D6,HIGH);
   while(digitalRead(D5) == HIGH){
@@ -30,12 +59,24 @@ if(digitalRead(D5) == HIGH){
    DataSerial.readBytes(value,2);
    rpm = atoi(value);
    Serial.println(rpm);
-   if(rpm == 3){
+   if(rpm == 3 || rpm == 4){
     digitalWrite(D7,HIGH);
     digitalWrite(D6,LOW);
     delay(1000);
     digitalWrite(D7,LOW);
    }
+  }
+  if(rpm == 3){
+    time_t now = time(nullptr);
+  String nazwa_otwierajacego = "Jakub";
+  String data_otwierajacy = ctime(&now) + nazwa_otwierajacego;
+  Firebase.pushString("Historia-Kart", data_otwierajacy);
+  }else if(rpm == 4)
+  {
+    time_t now = time(nullptr);
+  String nazwa_otwierajacego = "Agnieszka";
+  String data_otwierajacy = ctime(&now) + nazwa_otwierajacego;
+  Firebase.pushString("Historia-Kart", data_otwierajacy);
   }
 }
 
